b8addfb02216f3a113837859751abcbe44253009
nickthegit <nick@rosielee.co.uk>
2020-09-22 17:21:50 +0100

    holding page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,57 @@
 <template>
   <div>
-    <main-header />
+    <!-- <main-header /> -->
+    <!-- <header>
+      <nuxt-link to="/">
+        <Logo />
+      </nuxt-link>
+    </header>-->
     <Nuxt />
-    <main-footer />
+    <!-- <footer>
+      <a href="tel:+44 7974 449 746">Call Now: 07974 449 746</a>
+    </footer>-->
+    <!-- <main-footer /> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
+  div {
+  }
+
+  header {
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: chartreuse;
+    a {
+      width: 80px;
+      position: relative;
+      display: block;
+      margin: 20px;
+      // top: 20px;
+      z-index: 99;
+    }
+  }
+  footer {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    padding: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    a {
+      display: inline-block;
+      background: $primary;
+      padding: 20px;
+      border-radius: 8px;
+      text-decoration: none;
+      text-transform: uppercase;
+      font-size: 22px;
+      letter-spacing: 1px;
+      color: $white;
+    }
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
   <main>
-    <h1>Dave</h1>
+    <section>
+      <Logo />
+      <div class="backdrop"></div>
+      <h1>Bagwell Plumbing Services</h1>
+      <p>
+        Covering the Sherborne, Yeovil and surrounding areas, Bagwell Plumbing provide all plumbing and heating services including New Installations, Refurbs, Repairs, Bathrooms and Heating.
+        <br />
+        <br />So if you've got a leak, Baggy will take a peak!
+      </p>
+      <div class="links">
+        <a href="tel:+44 7974 449 746" target="_blank" rel="noopener noreferrer">tel: 07974 449 746</a>
+        <br />
+        <a
+          href="mailto:bagwellplumbingandheating@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >email</a>
+        <a
+          href="https://www.instagram.com/bagwellplumbing/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >the grams</a>
+      </div>
+      <h2>Website coming soon!</h2>
+    </section>
+    <!-- <section class="hero">
+      <div class="hero__copy">
+        <h1>Sprung a leak? I'll take a peak</h1>
+      </div>
+      <div class="hero__img">
+        <img src="https://via.placeholder.com/1450x550" alt />
+      </div>
+    </section>-->
   </main>
 </template>
 
@@ -17,4 +49,79 @@
 </script>
 
 <style lang="scss" scoped>
+  main {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+  }
+  section {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    padding: 40px;
+    color: $white;
+    @include breakpoint(mobile) {
+      padding: 20px;
+    }
+  }
+  svg,
+  img {
+    width: 300px;
+    padding: 20px;
+    flex-grow: 2;
+    @include breakpoint(mobile) {
+      width: 260px;
+    }
+  }
+  h1 {
+    font-size: 36px;
+    @include breakpoint(mobile) {
+      font-size: 28px;
+    }
+  }
+  h2 {
+    font-size: 28px;
+    padding-top: 20px;
+    @include breakpoint(mobile) {
+      font-size: 22px;
+    }
+  }
+  p {
+    width: 100%;
+    max-width: 600px;
+    font-size: 16px;
+  }
+  .links {
+    padding: 20px;
+    border-bottom: 2px solid $white;
+    a {
+      display: inline-block;
+      font-family: 'Russo One', sans-serif;
+      font-size: 20px;
+      padding: 5px;
+      color: $white;
+    }
+  }
+  .backdrop {
+    width: 100%;
+    height: calc(100% - 300px);
+    position: absolute;
+    bottom: 0;
+    background: rgb(48, 41, 131);
+    background: linear-gradient(
+      315deg,
+      rgba(48, 41, 131, 1) 0%,
+      rgba(27, 23, 71, 1) 100%
+    );
+    z-index: -1;
+    @include breakpoint(mobile) {
+      height: calc(100% - 220px);
+    }
+  }
 </style>
